159867d0d061897c3ac5a2ca7a8fc6680bef824c
Oleg Teplitsky <arthadi@gmail.com>
2018-07-09 14:19:38 +0300

    html

--- src/js/main.js
+++ src/js/main.js
@@ -72,3 +72,32 @@ $(document).ready(function(){
     });
 });
 
+$(function () {
+    const $button_hidden = $(".js-hide-menu");
+    const $menu_main = $(".js-menu__main");
+
+    $button_hidden.on("click", function () {
+
+        if ($menu_main.height() === 0) {
+            var height_menu = $menu_main.css({"height":"auto"}).outerHeight();
+            $menu_main.css({"height":"0"});
+            $menu_main.animate({"height": height_menu}, 500);
+            return false;
+        }
+        else {
+            $menu_main.animate({"height": 0}, 500);
+            return false;
+        }
+
+    });
+
+    $(window).on("resize", function () {
+        var width_window = $(window).width();
+
+        if (width_window > 551) {
+            $menu_main.attr("style", ' ');
+        }
+
+    });
+});
+
